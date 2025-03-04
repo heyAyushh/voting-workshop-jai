b640023b43023880cfed66971e98ad7ed5e6f84b
Aditya <147301021+adityajha2005@users.noreply.github.com>
2025-03-04 11:26:03 +0530

    function  increments the candidate amount counter

--- anchor/programs/voting/src/lib.rs
+++ anchor/programs/voting/src/lib.rs
@@ -28,8 +28,13 @@ pub mod voting {
                                 _poll_id: u64
                             ) -> Result<()> {
         let candidate = &mut ctx.accounts.candidate;
+        let poll = &mut ctx.accounts.poll;
+        
         candidate.candidate_name = candidate_name;
         candidate.candidate_votes = 0;
+        
+        poll.candidate_amount += 1;
+        
         Ok(())
     }
 
